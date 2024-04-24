1620888b77edf9021beedb6560f05d26c2e114cf
ikhlashmulya <nurahmanmulya@gmail.com>
2024-04-24 13:47:28 +0700

    fixed it

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,28 +1,19 @@
 import path from "path";
 import ytdl from "ytdl-core";
 import ffmpeg from "fluent-ffmpeg";
-import fs from "fs";
+import {Context} from "telegraf";
 
-export async function download(videoId: string): Promise<string> {
+export async function download(ctx: Context, videoId: string): Promise<void> {
 	const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`);
 	
-	const fileName = `${info.videoDetails.title}.mp3`;
-	//	const outputFilePath = path.join(__dirname+"/../public/atarayo.mp3");
+	const fileName = `${info.videoDetails.title.replace(/[\s/]/g, "_")}.mp3`;
+	const outputFilePath = path.join("public/", fileName);
 	const stream = ytdl(`https://www.youtube.com/watch?v=${videoId}`);
-//	const outputFile = fs.createWriteStream(outputFilePath); 
 
-	/*
-	ffmpeg(stream)
-	.toFormat("mp3")
-	.on("error", err => console.log("err : "+err))
-	.on("end", () => console.log("finished"))
-	.pipe(outputFile)
-	.on("error", err => console.log("err : "+err))
-	.on("finished", () => console.log("file saved"));
-	*/ 
-
- ffmpeg(stream).audioBitrate(128).save(path.join(__dirname, "..", "public", "atarayo.mp3")).on("end", () => console.log("file saved")).on("error", err => console.log("err : "+ err));
-
-	return fileName;
+	ffmpeg(stream).audioBitrate(128).save(outputFilePath)
+	.on("end", () => {
+		console.log("file saved sending to user...")
+		ctx.replyWithAudio({source: outputFilePath});
+	}).on("error", err => console.log("err : "+ err));
 }
 
